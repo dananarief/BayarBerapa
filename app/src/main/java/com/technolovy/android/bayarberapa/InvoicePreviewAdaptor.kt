ce5ab4d31c4e93040f5182a4b944454a7935f44e
dananarief <dananarief@gmail.com>
2020-02-08 14:38:09 +0700

    remove warning on InvoicePreviewAdaptor

--- app/src/main/java/com/technolovy/android/bayarberapa/InvoicePreviewAdaptor.kt
+++ app/src/main/java/com/technolovy/android/bayarberapa/InvoicePreviewAdaptor.kt
@@ -3,7 +3,6 @@ package com.technolovy.android.bayarberapa
 import android.content.Context
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
@@ -14,14 +13,13 @@ import com.technolovy.android.bayarberapa.helper.inflate
 import com.technolovy.android.bayarberapa.model.InvoiceItem
 import kotlinx.android.synthetic.main.invoice_list_preview_item.view.*
 
-class InvoicePreviewAdaptor(var invoiceItems: ArrayList<InvoiceItem>): RecyclerView.Adapter<InvoicePreviewAdaptor.InvoiceHolder>(), AdapterView.OnItemSelectedListener {
+class InvoicePreviewAdaptor(private var invoiceItems: ArrayList<InvoiceItem>): RecyclerView.Adapter<InvoicePreviewAdaptor.InvoiceHolder>(), AdapterView.OnItemSelectedListener {
     var onTypeChange: ((Int,InvoiceItem.InvoiceType)->Unit)? = null
     override fun onNothingSelected(parent: AdapterView<*>?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        Log.d("click spinner", "value ${parent?.getItemAtPosition(position)}")
 
     }
 
@@ -35,7 +33,7 @@ class InvoicePreviewAdaptor(var invoiceItems: ArrayList<InvoiceItem>): RecyclerV
     }
 
     override fun onBindViewHolder(holder: InvoiceHolder, position: Int) {
-        var invoiceItem = invoiceItems[position]
+        val invoiceItem = invoiceItems[position]
         holder.bindInvoiceItem(invoiceItem)
         holder.itemView.spinner_invoice_type.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -49,11 +47,10 @@ class InvoicePreviewAdaptor(var invoiceItems: ArrayList<InvoiceItem>): RecyclerV
                 id: Long
             ) {
                 val selectedDropdown = parent?.getItemAtPosition(position).toString()
-                val enumBasedOnDropdown = InvoiceItem.InvoiceType.values().firstOrNull() {
+                val enumBasedOnDropdown = InvoiceItem.InvoiceType.values().firstOrNull {
                     it.displayName == selectedDropdown
                 }
                 invoiceItem.type = enumBasedOnDropdown
-                Log.d("dropdown change","into ${invoiceItem.type}")
                 enumBasedOnDropdown?.let {
                     onTypeChange?.invoke(position,enumBasedOnDropdown)
                 }
@@ -70,8 +67,7 @@ class InvoicePreviewAdaptor(var invoiceItems: ArrayList<InvoiceItem>): RecyclerV
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                Log.d("editTextprice","edit message into ${s}")
-                var priceInDouble = extractPriceToDouble(s.toString())
+                val priceInDouble = extractPriceToDouble(s.toString())
                 priceInDouble?.let {
                     invoiceItem.price = it
                 }
@@ -89,33 +85,29 @@ class InvoicePreviewAdaptor(var invoiceItems: ArrayList<InvoiceItem>): RecyclerV
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                Log.d("editTextitem","edit message into ${s}")
                 invoiceItem.name = s.toString()
             }
 
         })
     }
 
-    class InvoiceHolder(private var view: View, var context: Context): RecyclerView.ViewHolder(view) {
+    class InvoiceHolder(private var view: View, private var context: Context): RecyclerView.ViewHolder(view) {
         private var item: InvoiceItem? = null
-        private var spinnerAdapter: ArrayAdapter<CharSequence>? = null
 
         fun bindInvoiceItem(invoiceItem: InvoiceItem) {
             this.item = invoiceItem
             view.item_text.setText(invoiceItem.name)
             view.price_text.setText(invoiceItem.price.toString())
 
-            var spinnerAdapter = ArrayAdapter.createFromResource(
+            val spinnerAdapter = ArrayAdapter.createFromResource(
                 context,
                 R.array.invoice_type_array,
                 R.layout.custom_spinner_item)
             spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
             view.spinner_invoice_type.adapter = spinnerAdapter
-            val defaultValue = spinnerAdapter.getPosition(invoiceItem?.type?.displayName)
-            Log.d("spinner", "default value ${defaultValue}")
+            val defaultValue = spinnerAdapter.getPosition(invoiceItem.type?.displayName)
             view.spinner_invoice_type.setSelection(defaultValue)
-            Log.d("test view", "bindinv item ${view.item_text.text} ${view.price_text.text}")
         }
 
     }
