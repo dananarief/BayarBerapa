5ccbd5fee0073bc500b71a1dda52a226fea569cb
dananarief <dananarief@gmail.com>
2020-02-16 13:02:50 +0700

    clear any info when user clear image

--- app/src/main/java/com/technolovy/android/bayarberapa/MainActivity.kt
+++ app/src/main/java/com/technolovy/android/bayarberapa/MainActivity.kt
@@ -86,6 +86,10 @@ class MainActivity : AppCompatActivity() {
             if (invoice != null) {
                 invoice = null
                 invoiceImageUri = null
+                InvoiceManager.invoiceImg = null
+                InvoiceManager.recipientList.clear()
+                InvoiceManager.firebaseVisionText = null
+                InvoiceManager.invoiceOnScreen = null
                 render()
             }
         }
@@ -96,6 +100,7 @@ class MainActivity : AppCompatActivity() {
         invoice?.numOfPerson = person
         InvoiceManager.invoiceOnScreen = invoice
         InvoiceManager.firebaseVisionText = firebaseVisionText
+        InvoiceManager.invoiceImg = invoiceImageUri
 
         startActivityForResult(intent, GO_TO_PREVIEW_SCREEN)
     }
--- app/src/main/java/com/technolovy/android/bayarberapa/helper/InvoiceManager.kt
+++ app/src/main/java/com/technolovy/android/bayarberapa/helper/InvoiceManager.kt
@@ -1,5 +1,6 @@
 package com.technolovy.android.bayarberapa.helper
 
+import android.net.Uri
 import com.google.firebase.ml.vision.text.FirebaseVisionText
 import com.technolovy.android.bayarberapa.model.InvoiceITF
 import com.technolovy.android.bayarberapa.model.Recipient
@@ -8,4 +9,5 @@ object InvoiceManager {
     var invoiceOnScreen: InvoiceITF? = null
     var firebaseVisionText: FirebaseVisionText? = null
     var recipientList = ArrayList<Recipient>()
+    var invoiceImg: Uri? = null
 }
